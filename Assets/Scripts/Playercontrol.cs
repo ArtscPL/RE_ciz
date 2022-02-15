79b77f039209483c5cd3d9cb464c87d45febf1f0
ArtscPL <92299930+ArtscPL@users.noreply.github.com>
2022-02-16 03:35:01 +0700

    Random Number

--- Assets/Scripts/Playercontrol.cs
+++ Assets/Scripts/Playercontrol.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Playercontrol : MonoBehaviour
 {
     [Range(0.0f, 100.0f)] public float Health = 100f;
+    [SerializeField] private GameObject player = null;
+    [SerializeField] private GameObject Enemy = null;
+    [SerializeField] private Slider player_hp = null;
+    private int enemy_hp;
+    private Random_Enemy_HP R_hp;
     void Start()
     {
-        
+        R_hp = FindObjectOfType<Random_Enemy_HP>();
+        enemy_hp = R_hp.HP;
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Random_Number_Number.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Random_Number_Number : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TMP_Text showText;
+    void Start()
+    {
+        Random_Number_Marble(0,9);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Random_Number_Marble(int min,int max){
+        int Number_Marble = Random.Range(min,max+1);
+        showText.text = Number_Marble.ToString();
+    }
+}
