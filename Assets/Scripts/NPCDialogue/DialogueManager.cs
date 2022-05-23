d9d1245e92fa90f2fb9fa666cbf72f6213793808
ChickenLoner <warawut_manosong@hotmail.com>
2022-05-23 21:05:25 +0700

    AddBossTrigger

--- Assets/Scripts/NPCDialogue/DialogueManager.cs
+++ Assets/Scripts/NPCDialogue/DialogueManager.cs
@@ -160,7 +160,10 @@ public class DialogueManager : MonoBehaviour
     }
 
     public void BossFightInit(){
-        
+        DialogueManager.GetInstance().SetVariableState("fightBoss", new Ink.Runtime.BoolValue(false));
+        SaveManager.GetInstance().SavetoAsset();
+        SaveManager.GetInstance().SaveGameData();
+        BossManager.GetInstance().TriggerBoss();
     }
 
     private void QuestWindowPopup()
---
+++ Assets/Scripts/NPCDialogue/TestBoss.ink
@@ -0,0 +1,9 @@
+INCLUDE globals.ink
+
+->testfight
+
+=== testfight ===
+เริ่มสู้กับบอสได้
+อิๆ 
+~fightBoss = true
+->DONE
