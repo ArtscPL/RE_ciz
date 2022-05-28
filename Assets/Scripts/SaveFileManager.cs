a362d3ede878c1ea5768e36336c7c5e59f37200b
ChickenLoner <warawut_manosong@hotmail.com>
2022-05-28 23:13:03 +0700

    Patch_update_Save

--- Assets/Scripts/SaveFileManager.cs
+++ Assets/Scripts/SaveFileManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+using System.IO;
 
 public class SaveFileManager : MonoBehaviour
 {
+    public GameObject SaveUI;
     public Button Button1;
     public TextMeshProUGUI text1;
     public Button Button2;
     public TextMeshProUGUI text2;
     public Button Button3;
     public TextMeshProUGUI text3;
+
+    public string path1;// = Application.persistentDataPath + "/saves/" + "slot1" + ".rez";
+    public string path2;// = Application.persistentDataPath + "/saves/" + "slot2" + ".rez";
+    public string path3;// = Application.persistentDataPath + "/saves/" + "slot3" + ".rez";
     // Start is called before the first frame update
     void Awake()
     {
@@ -22,6 +27,9 @@ public class SaveFileManager : MonoBehaviour
 		b2.onClick.AddListener(ClickSave2);
         Button b3 = Button3.GetComponent<Button>();
 		b3.onClick.AddListener(ClickSave3);
+        path1 = Application.persistentDataPath + "/saves/" + "slot1" + ".rez";
+        path2 = Application.persistentDataPath + "/saves/" + "slot2" + ".rez";
+        path3 = Application.persistentDataPath + "/saves/" + "slot3" + ".rez";
     }
 
     // Update is called once per frame
@@ -29,6 +37,18 @@ public class SaveFileManager : MonoBehaviour
     {
         
     }*/
+
+    void Update(){
+        if(SaveUI!=null && SaveUI.activeSelf){
+            if(File.Exists(path1))
+            text1.text = "Save 1 - Existed";
+            if(File.Exists(path2))
+            text2.text = "Save 2 - Existed";
+            if(File.Exists(path3))
+            text3.text = "Save 3 - Existed";
+        }
+    }
+
 
     public void ClickSave1(){
         SFXmanager.GetInstance().ButtonPressSound();
